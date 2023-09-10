ed7d8081b11dfa0fa35466901358cec05e272bc4
oscarkremer <oscar.s.kremer@hotmail.com>
2023-09-10 15:52:43 -0300

    feat: add notebook explaining about traces and gain conversion

--- setup.py
+++ setup.py
@@ -9,24 +9,19 @@ import os
 PROJECT_DIR = os.path.dirname(__file__)
 
 setup(
-     name='non_linearity',  
-     version='0.0.1',
-     author='Quantum Adventures',
-     author_email='laboratorio.quantica.cetuc@gmail.com',
-     description='short description here',
-     long_description=long_description,
-     long_description_content_type='text/markdown',
-     license=open(os.path.join(PROJECT_DIR, 'LICENSE')).read(),
-     packages=find_packages(),
-     url='',
-     classifiers=[
-         'Programming Language :: Python :: 3',
-         'Operating System :: OS Independent',
-     ],
-    install_requires=[
-        'numpy',
-        'pytest',
-        'matplotlib',
-        'seaborn'
-    ]
+    name="non_linearity",
+    version="0.0.1",
+    author="Quantum Adventures",
+    author_email="laboratorio.quantica.cetuc@gmail.com",
+    description="short description here",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license=open(os.path.join(PROJECT_DIR, "LICENSE")).read(),
+    packages=find_packages(),
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "pytest", "matplotlib", "seaborn"],
 )
